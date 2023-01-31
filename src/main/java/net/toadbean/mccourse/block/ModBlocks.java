3af1607ecf15bc035e11608a6bf1ca535966d63f
Toadbean <tobiascodesgames@gmail.com>
2023-01-31 21:53:07 +0000

    Lecture 35

--- src/main/java/net/toadbean/mccourse/block/ModBlocks.java
+++ src/main/java/net/toadbean/mccourse/block/ModBlocks.java
@@ -15,13 +15,13 @@ import net.toadbean.mccourse.item.ModItemGroups;
 public class ModBlocks {
 
     public static final Block ORICHALCUM_BLOCK = registerBlock("orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(2f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroups.COURSE);
     public static final Block ORICHALCUM_ORE = registerBlock("orichalcum_ore",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroups.COURSE);
     public static final Block RAW_ORICHALCUM_BLOCK = registerBlock("raw_orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroups.COURSE);
     public static final Block DEEPSLATE_ORICHALCUM_ORE = registerBlock("deepslate_orichalcum_ore",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroups.COURSE);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
